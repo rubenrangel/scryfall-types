eb8819f39e7d8063b6ec863e64816071ce59ca42
Ruben Rangel <rubenmrangel@gmail.com>
2023-04-01 08:04:56 -0700

    chore: add .idea folder to .gitignore

--- .projenrc.js
+++ .projenrc.js
@@ -13,5 +13,8 @@ const project = new typescript.TypeScriptProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  gitignore: [
+    '.idea',
+  ],
 });
 project.synth();
