1eaf245ebfaefef4b4e45150f3e88f06fc44b2b5
Ruben Rangel <rubenmrangel@gmail.com>
2023-04-01 08:10:52 -0700

    chore: add `build` and `ci` as valid PR types

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,13 @@ const { typescript } = require('projen');
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'scryfall-types',
-
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ['build', 'feat', 'fix', 'chore', 'ci'],
+      },
+    },
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
