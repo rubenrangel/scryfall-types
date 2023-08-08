31ad11c2620864a99ea56de264cc99ed376f7ee5
Ruben Rangel <rubenmrangel@gmail.com>
2023-08-07 20:50:57 -0700

    feat: add `IScryfallCardGameplayFields` interface

--- src/IScryfallCard.ts
+++ src/IScryfallCard.ts
@@ -1,4 +1,7 @@
+import { IScryfallCardFace } from "./IScryfallCardFace";
+import { IScryfallColor } from "./IScryfallColor";
 import { IScryfallObject } from "./IScryfallObject";
+import { IScryfallRelatedCard } from "./IScryfallRelatedCard";
 
 export interface IScryfallCardCoreFields {
   /**
@@ -87,12 +90,145 @@ export interface IScryfallCardCoreFields {
   uri: string;
 }
 
+export interface IScryfallCardGameplayFields {
+  /**
+   * If this card is closely related to other cards, this property will be an
+   * array with Related Card Objects.
+   */
+  all_parts?: Array<IScryfallRelatedCard> | null;
+
+  /**
+   * An array of Card Face objects, if this card is multifaced.
+   */
+  card_faces?: Array<IScryfallCardFace> | null;
+
+  /**
+   * The card's converted mana cost. Note that some funny cards have fractional
+   * mana costs.
+   */
+  cmc: number;
+
+  /**
+   * This card's color identity.
+   */
+  color_identity: Array<IScryfallColor>;
+
+  /**
+   * The colors in this card's color indicator, if any. A null value for this
+   * field indicates the card does not have one.
+   */
+  color_indicator?: Array<IScryfallColor> | null;
+
+  /**
+   * This card's colors, if the overall card has colors defined by the rules.
+   * Otherwise the colors will be on the card_faces objects, see below.
+   */
+  colors?: Array<IScryfallColor> | null;
+
+  /**
+   * This card's overall rank/popularity on EDHREC. Not all cards are ranked.
+   */
+  edh_rank?: number | null;
+
+  /**
+   * This card's hand modifier, if it is Vanguard card. This value will contain
+   * a delta, such as `'-1'`.
+   */
+  hand_modifier?: string | null;
+
+  /**
+   * An array of keywords that this card uses, such as `'Flying'` and
+   * `'Cumulative upkeep'`.
+   */
+  keywords: Array<string>;
+
+  /**
+   * A code for this card's layout.
+   */
+  layout: string;
+
+  /**
+   * An object describing the legality of this card across play formats.
+   * Possible legalities are `legal`, `not_legal`, `restricted`, and `banned`.
+   */
+  legalities: Record<string, "legal" | "not_legal" | "restricted" | "banned">;
+
+  /**
+   * This card's life modifier, if it is Vanguard card. This value will contain
+   * a delta, such as `+2`.
+   */
+  life_modifier?: string | null;
+
+  /**
+   * This loyalty if any. Note that some cards have loyalties that are not
+   * numeric, such as `X`.
+   */
+  loyalty?: string | null;
+
+  /**
+   * The mana cost for this card. This value will be any empty string "" if the
+   * cost is absent. Remember that per the game rules, a missing mana cost and a
+   * mana cost of `{0}` are different values. Multi-faced cards will report this
+   * value in card faces.
+   */
+  mana_cost?: string | null;
+
+  /**
+   * The name of this card. If this card has multiple faces, this field will
+   * contain both names separated by `␣//␣`.
+   */
+  name: string;
+
+  /**
+   * The Oracle text for this card, if any.
+   */
+  oracle_text?: string | null;
+
+  /**
+   * True if this card is oversized.
+   */
+  oversized: boolean;
+
+  /**
+   * This card’s rank/popularity on Penny Dreadful. Not all cards are ranked.
+   */
+  penny_rank?: number | null;
+
+  /**
+   * This card's power, if any. Note that some cards have powers that are not
+   * numeric, such as `*`.
+   */
+  power?: string | null;
+
+  /**
+   * Colors of mana that this card could produce.
+   */
+  produced_mana?: Array<IScryfallColor> | null;
+
+  /**
+   * True if this card is on the Reserved List.
+   */
+  reserved: boolean;
+
+  /**
+   * This card's toughness, if any. Note that some cards have toughnesses that
+   * are not numeric, such as `*`.
+   */
+  toughness?: string | null;
+
+  /**
+   * The type line of this card.
+   */
+  type_line: string;
+}
+
 /**
  * Card objects represent individual Magic: The Gathering cards that players
  * could obtain and add to their collection (with a few minor exceptions).
  */
 export interface IScryfallCard
   extends IScryfallObject,
-    IScryfallCardCoreFields {
+    IScryfallCardCoreFields,
+    IScryfallCardGameplayFields {
   object: "card";
 }
